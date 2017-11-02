e475809f1b06f52cda32c9888b127dd355f82da8
Victor Le Pen <victor@yarilabs.com>
2017-11-02 16:20:00 +0000

    removed smooth scoolling, doesn't work on chrome ver62.0.3202.75 above(?)

--- assets/sites/insig/theme/javascripts/theme/Zoo/script49749c43.js
+++ assets/sites/insig/theme/javascripts/theme/Zoo/script49749c43.js
@@ -20,6 +20,8 @@ jQuery(document).ready(function($) {
 	
 	/*Active Item Highlight Bar*/
 	function activeNav() {
+		//console.log(alert("olá, funciono?"));
+		//console.log($(".nav-active"));
 		xMove = $(".nav-active").offset().left;
 		barWidth =  $(".nav-active").outerWidth() + 'px';
 		$('#active-bar').css({ left: xMove, width: barWidth }); 
@@ -38,13 +40,14 @@ jQuery(document).ready(function($) {
  	$('.nav-links li.page-section-parent > a').removeClass('external');
 	
 	/*Smooth Scrolling*/
-	$('.nav-links').onePageNav({
+	/*$('.nav-links').onePageNav({
 		currentClass: 'nav-active',
 		scrollThreshold: 0.5,
 		scrollSpeed: 800,
 		scrollOffset: 54,
 		filter: ':not(.external)'
-	}); 
+	}); */
+
 	if($('body').hasClass('home')){
 		$('.mainLogo a, .footerLogo').click(function(event){
 			 event.preventDefault();
